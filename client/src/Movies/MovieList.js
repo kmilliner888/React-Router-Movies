4797faa4e93acade63e730c57b182c8978124267
Kaylen Milliner <kaylen.milliner@gmail.com>
2020-02-16 00:35:46 -0500

    fixed Link component and onClick

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -37,22 +37,25 @@ const MovieList = props => {
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
-    <div className="movie-card" >
-      <Link to={`/movies/${movie.id}`} onClick={movie.id}></Link>
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
+    <div className="movie-card">
+      <Link to={`/movies/${movie.id}`}  onClick={()=>movie}>
+        
+          <h2>{title}</h2>
+          <div className="movie-director">
+            Director: <em>{director}</em>
+          </div>
+          <div className="movie-metascore">
+            Metascore: <strong>{metascore}</strong>
+          </div>
+          <h3>Actors</h3>
 
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
+          {stars.map(star => (
+            <div key={star} className="movie-star">
+              {star}
+            </div>
+          ))}
+
+      </Link>
     </div>
   );
 }
